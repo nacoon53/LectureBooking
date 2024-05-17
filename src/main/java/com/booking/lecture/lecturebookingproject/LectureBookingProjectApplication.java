eb4eaab1108a441af0f435f6a98e85d81f73dc17
nakyoung <nadiyacoon@gmail.com>
2024-05-17 20:38:18 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/booking/lecture/lecturebookingproject/LectureBookingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.booking.lecture.lecturebookingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureBookingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LectureBookingProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/booking/lecture/lecturebookingproject/LectureBookingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booking.lecture.lecturebookingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureBookingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
